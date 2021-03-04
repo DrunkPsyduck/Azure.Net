c28c83219aa95b8a5cc3ef5848184dabf3dd34d0
Mario Canales <Mario.Canales@tajamar365.com>
2021-03-04 11:50:00 +0100

    Se crea el proyecto del NuGet de Doctores y el cliente para que lo consuma

---
+++ ApiCrudDoctor/ApiCrudDoctor/Controllers/DoctoresController.cs
@@ -0,0 +1,23 @@
+﻿using ApiCrudDoctor.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCrudDoctor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctoresController : ControllerBase
+    {
+        RepositoryDoctores repo;
+        public DoctoresController(RepositoryDoctores repo)
+        {
+            this.repo = repo;
+        }
+
+
+    }
+}
---
+++ ApiCrudDoctor/ApiCrudDoctor/Data/DoctoresContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NugetDoctorModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCrudDoctor.Data
+{
+    public class DoctoresContext : DbContext
+    {
+        public DoctoresContext(DbContextOptions<DoctoresContext> options) : base(options) { }
+        public DbSet<Doctor> Doctores { get; set; }
+    }
+}
