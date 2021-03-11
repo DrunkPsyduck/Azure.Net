32c5246064651785b44ceab6bdfbf8fa88644acb
Mario <canalessanchezmario@gmail.com>
2021-03-11 10:47:27 +0100

    Se termina el desarrollo del formulario para migrar los datos de un xml a un storage(desarrollo) en azure. Se crea un proyecto nuevo 'ApiTokenAlumnos' para aplicar token SaS a las tablas de Azure.

---
+++ MigracionTablasAzure/MigracionTablasAzure/Alumno.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigracionTablasAzure
+{
+    public class Alumno : TableEntity
+    {
+        private String _IdAlumno;
+        public String IdAlumno
+        {
+            get { return this._IdAlumno; }
+            set
+            {
+                this._IdAlumno = value;
+                this.RowKey = value;
+            }
+        
+        }
+
+        private String _Curso;
+        public String curso
+        {
+            get { return this._Curso; }
+            set
+            {
+                this._Curso = value;
+                this.PartitionKey = value;
+            }
+        }
+
+        public String Nombre { get; set; }
+        public String Apellidos { get; set; }
+        public String Nota { get; set; }
+
+
+    }
+}
---
+++ MigracionTablasAzure/MigracionTablasAzure/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using Microsoft.Azure.Cosmos.Table;
+using System.IO;
+using System.Xml.Linq;
+
+namespace MigracionTablasAzure
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnmigrar_Click(object sender, EventArgs e)
+        {
+            String keys = ConfigurationSettings.AppSettings["storageaccountkey"];
+            CloudStorageAccount account = CloudStorageAccount.Parse(keys);
+            CloudTableClient client = account.CreateCloudTableClient();
+            CloudTable tabla = client.GetTableReference("tablaalumnos");//recursos de azure en minusculas
+            tabla.CreateIfNotExists();
+            Stream content = this.GetType().Assembly.GetManifestResourceStream("MigracionTablasAzure.alumnos_tables_storage.xml");
+            XDocument docxml = XDocument.Load(content);
+
+            var consulta = from datos in docxml.Descendants("alumno")
+                           select new Alumno
+                           {
+                               IdAlumno = datos.Element("idalumno").Value,
+                               Nombre = datos.Element("nombre").Value,
+                               curso = datos.Element("curso").Value,
+                               Apellidos = datos.Element("apellidos").Value,
+                               Nota = datos.Element("nota").Value
+                           };
+            foreach(Alumno alum in consulta)
+            {
+                TableOperation insert = TableOperation.Insert(alum);
+                tabla.Execute(insert);
+            }
+            this.lblmensaje.Text = "Todo OK";
+        }
+    }
+}
