37aec591342f47f071742baf7fc56f28758cf34f
Mario Canales <Mario.Canales@tajamar365.com>
2021-03-12 11:43:32 +0100

    Se añade la solucion al serviceBus para que muetre los mensajes. Se añaden los proyectos para los webjobs. Una vez probados se han eliminado de Azure

---
+++ TaskWebJob/TaskWebJob/Models/NoticiaRss.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskWebJob.Models
+{
+    public class NoticiaRss
+    {
+        public String Title { get; set; }
+        public String Link { get; set; }
+        public String Description { get; set; }
+    }
+}
---
+++ TaskWebJob/TaskWebJob/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using TaskWebJob.Data;
+using TaskWebJob.Repositories;
+
+namespace TaskWebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String cnn = "Data Source=sqlmarioc.database.windows.net;Initial Catalog=PROYECTO;User ID=adminsql;Password=Admin123";
+
+            var provider = new ServiceCollection().AddTransient<RepositoryWebJob>()
+                .AddDbContext<WebJobContext>(x => x.UseSqlServer(cnn)).BuildServiceProvider();
+            RepositoryWebJob repo = provider.GetService<RepositoryWebJob>();
+            //Console.WriteLine("Pulse una tecla para iniciar");
+            //Console.ReadLine();
+            repo.PopulateDataWebJob();
+            //Console.WriteLine("proceso terminado. Pulse Enter para finalizar");
+            //Console.ReadLine();
+        }
+    }
+}
